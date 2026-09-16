df152b8119b02d538ddc31d8380b99379d19323e
Khin Myat Thu <khinmyatthu2193@gmail.com>
2026-09-16 20:13:07 +0630

    chore: configure Supabase project dependencies

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 
 export default [
-  { ignores: ['dist', 'coverage', 'prisma/migrations'] },
+  { ignores: ['dist', 'coverage'] },
   js.configs.recommended,
   {
     files: ['**/*.js'],
